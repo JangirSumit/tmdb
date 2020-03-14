85372a5d92dd4c3c0570ff3e5aa0101ddd902fb1
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2020-03-14 18:24:26 +0530

    settings

--- src/components/settings.js
+++ src/components/settings.js
@@ -4,7 +4,19 @@ class Settings extends Component {
   onHomeClick(event) {
     this.props.onHomeClick();
   }
+
+  onLangChange(event) {
+    window.localStorage.setItem("lang", event.target.value);
+  }
+
+  onCountryChange(event) {
+    window.localStorage.setItem("country", event.target.value);
+  }
+
   render() {
+    let lang = window.localStorage.getItem("lang");
+    let country = window.localStorage.getItem("country");
+
     return (
       <div className="settings-page-wrapper">
         <div className="nav-bar">
@@ -18,15 +30,31 @@ class Settings extends Component {
         </div>
         <h2>Settings</h2>
         <p>Select Language</p>
-        <select id="language">
+        <select
+          id="language"
+          onChange={event => this.onLangChange(event)}
+          value={lang}
+        >
           {this.props.langs.map(l => {
-            return <option value={l.iso_639_1}>{l.english_name}</option>;
+            return (
+              <option key={l.iso_639_1} value={l.iso_639_1}>
+                {l.english_name}
+              </option>
+            );
           })}
         </select>
         <p>Select Country</p>
-        <select id="country">
+        <select
+          id="country"
+          onChange={event => this.onCountryChange(event)}
+          value={country}
+        >
           {this.props.countries.map(l => {
-            return <option value={l.iso_3166_1}>{l.english_name}</option>;
+            return (
+              <option key={l.iso_3166_1} value={l.iso_3166_1}>
+                {l.english_name}
+              </option>
+            );
           })}
         </select>
       </div>
