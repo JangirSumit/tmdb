7d75827a67945e43e63da16901a7597aa092ae23
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2020-04-01 11:54:16 +0530

    folder name change

---
+++ docs/precache-manifest.45c6e7d1f6cada817cfa42f5d578635c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132ec4dd0ca67de37d8be5255948bb56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b6f8ace61304157c22",
+    "url": "/static/css/main.2f278f8e.chunk.css"
+  },
+  {
+    "revision": "e244ceba8cb2b9d50141",
+    "url": "/static/js/2.b4973a52.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b4973a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b6f8ace61304157c22",
+    "url": "/static/js/main.198d74bd.chunk.js"
+  },
+  {
+    "revision": "bbba8d4907cb3e00814e",
+    "url": "/static/js/runtime-main.b45b50fe.js"
+  }
+]);
